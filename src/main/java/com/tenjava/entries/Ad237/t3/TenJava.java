490c8d7e357a79bc632c9e31c6c1be7fc0a1baf7
Ad237 <adamellaway@virginmedia.com>
2014-07-12 15:02:44 +0100

    Initial commit

--- src/main/java/com/tenjava/entries/Ad237/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/Ad237/t3/TenJava.java
@@ -2,4 +2,16 @@ package com.tenjava.entries.Ad237.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    private static TenJava instamce;
+
+    @Override
+    public void onEnable() {
+        instamce = this;
+    }
+
+    public static TenJava getInstamce() {
+        return instamce;
+    }
+}
