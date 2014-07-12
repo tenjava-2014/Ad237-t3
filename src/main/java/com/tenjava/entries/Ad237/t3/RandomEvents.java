e6b2feabac6b8e09cf470fe4e8cdbed77ec85a86
Ad237 <adamellaway@virginmedia.com>
2014-07-12 15:39:31 +0100

    Rename main class

--- src/main/java/com/tenjava/entries/Ad237/t3/RandomEvents.java
+++ src/main/java/com/tenjava/entries/Ad237/t3/RandomEvents.java
@@ -2,16 +2,16 @@ package com.tenjava.entries.Ad237.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {
+public class RandomEvents extends JavaPlugin {
 
-    private static TenJava instance;
+    private static RandomEvents instance;
 
     @Override
     public void onEnable() {
         instance = this;
     }
 
-    public static TenJava getInstance() {
+    public static RandomEvents getInstance() {
         return instance;
     }
 }
