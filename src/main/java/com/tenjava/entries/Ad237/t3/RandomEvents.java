b48adf47cf7bda076bc431c8ba411ef40dd4b321
Ad237 <adamellaway@virginmedia.com>
2014-07-12 15:43:33 +0100

    Get event by name

--- src/main/java/com/tenjava/entries/Ad237/t3/RandomEvents.java
+++ src/main/java/com/tenjava/entries/Ad237/t3/RandomEvents.java
@@ -1,10 +1,15 @@
 package com.tenjava.entries.Ad237.t3;
 
+import com.tenjava.entries.Ad237.t3.event.RandomEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
 
 public class RandomEvents extends JavaPlugin {
 
     private static RandomEvents instance;
+
+    private ArrayList<RandomEvent> randomEvents = new ArrayList<RandomEvent>();
 
     @Override
     public void onEnable() {
@@ -13,5 +18,18 @@ public class RandomEvents extends JavaPlugin {
 
     public static RandomEvents getInstance() {
         return instance;
+    }
+
+    public ArrayList<RandomEvent> getRandomEvents() {
+        return randomEvents;
+    }
+
+    public RandomEvent getRandomEvent(String name) {
+        for(RandomEvent event : getRandomEvents()) {
+            if(event.getName().equalsIgnoreCase(name)) {
+                return event;
+            }
+        }
+        return null;
     }
 }
