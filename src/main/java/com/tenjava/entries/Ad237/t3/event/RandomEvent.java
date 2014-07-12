a1ecd2595f6d0cdb17a3a15fe6e2713701668689
Ad237 <adamellaway@virginmedia.com>
2014-07-12 15:56:27 +0100

    Add chance logic

--- src/main/java/com/tenjava/entries/Ad237/t3/event/RandomEvent.java
+++ src/main/java/com/tenjava/entries/Ad237/t3/event/RandomEvent.java
@@ -2,14 +2,20 @@ package com.tenjava.entries.Ad237.t3.event;
 
 import org.bukkit.event.Listener;
 
+import java.util.Random;
+
 public abstract class RandomEvent implements Listener {
 
+    private final static Random rand = new Random();
+
     private String name;
+    private double chance;
     private String[] description;
     private boolean enabled;
 
-    public RandomEvent(String name, boolean enabled, String... description) {
+    public RandomEvent(String name, double chance, boolean enabled, String... description) {
         this.name = name;
+        this.chance = chance;
         this.enabled = enabled;
         this.description = description;
     }
@@ -28,6 +34,14 @@ public abstract class RandomEvent implements Listener {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public double getChance() {
+        return chance;
+    }
+
+    public boolean shouldHappen() {
+        return rand.nextDouble() < getChance();
     }
 
 }
